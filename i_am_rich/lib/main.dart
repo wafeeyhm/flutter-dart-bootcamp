8b47ebbb9d56cc7b0dbf9a7b17d2633fc4cb480b
Wafeey HM <wafiy@anak.it>
2022-06-22 16:49:15 +0800

    i-am-rich-part-1
    Currently working on 26 Scaffolding a flutter app

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
 
+//The main function is the starting point for all our Flutter apps.
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text("Hello World"),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('I am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        backgroundColor: Colors.blueGrey[900],
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg'),
+          ),
+        ),
       ),
     ),
   );
